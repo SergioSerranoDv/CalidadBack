900d342050886a4c3c00c5dc5445fe1f84283910
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-05-27 21:32:22 -0500

    chore: Update usuarioRoutes to include endpoint for updating parameters

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const router = express.Router();
-const checkAuth = require('../../middleware/checkAuth');
-const { 
-    registrar,
-    autenticar,
-    confirmar,
-    olvidePassword,
-    comprobarToken,
-    nuevoPassword,
-    perfil,
-    obtenerPerfil,
-} = require("../controllers/usuarioController");
-router.post("/", registrar);
-router.post("/login", autenticar);
-router.get("/confirmar/:token", confirmar);
-router.post('/olvide-password', olvidePassword);
-router.get('/olvide-password/:token', comprobarToken);
-router.post('/olvide-password/:token', nuevoPassword);
-router.get('/perfil', checkAuth, perfil);
-router.get('/perfil/:id', obtenerPerfil);
-module.exports = router;
+const express = require("express")
+const router = express.Router()
+const checkAuth = require("../../middleware/checkAuth")
+const {
+  registrar,
+  autenticar,
+  confirmar,
+  olvidePassword,
+  comprobarToken,
+  nuevoPassword,
+  perfil,
+  obtenerPerfil,
+  updateParameters,
+} = require("../controllers/usuarioController")
+router.post("/", registrar)
+router.post("/login", autenticar)
+router.get("/confirmar/:token", confirmar)
+router.post("/olvide-password", olvidePassword)
+router.get("/olvide-password/:token", comprobarToken)
+router.post("/olvide-password/:token", nuevoPassword)
+router.get("/perfil", checkAuth, perfil)
+router.get("/perfil/:id", obtenerPerfil)
+router.put("/upate/parameters", checkAuth, updateParameters)
+module.exports = router
